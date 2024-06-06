e2908c9ed325b7bf2bc58eed040f9c70879d8cbd
JohnGastone <johngastone11@gmail.com>
2024-06-06 18:48:34 +0300

    +++

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -101,28 +101,31 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Container(
-                    height: 120,
-                    width: 80,
+                    height: 150,
+                    width: 100,
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      children: [
-                        Icon(
-                          CupertinoIcons.hand_draw,
-                          color: Color.fromARGB(255, 28, 39, 81),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "Design",
-                          style: GoogleFonts.spaceMono(
-                            fontSize: 18,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Column(
+                        children: [
+                          Icon(
+                            CupertinoIcons.hand_draw,
                             color: Color.fromARGB(255, 28, 39, 81),
                           ),
-                        )
-                      ],
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            "Design",
+                            style: GoogleFonts.spaceMono(
+                              fontSize: 18,
+                              color: Color.fromARGB(255, 28, 39, 81),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   )
                 ],
