4d4bd0eabe0f3acf074b8bca8a873b1eceeccd73
JohnGastone <johngastone11@gmail.com>
2024-06-06 23:15:41 +0300

    +++

--- lib/screens/courseDetail.dart
+++ lib/screens/courseDetail.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Coursedetails extends StatefulWidget {
   const Coursedetails({super.key});
@@ -17,26 +18,20 @@ class _CoursedetailsState extends State<Coursedetails> {
         // ignore: prefer_const_literals_to_create_immutables
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               // ignore: prefer_const_literals_to_create_immutables
               children: [
                 CircleAvatar(
-                  child: IconButton(
-                    icon: Icon(
+                    backgroundColor: const Color.fromARGB(60, 214, 209, 209),
+                    child: Icon(
                       Icons.arrow_back,
                       color: Color.fromARGB(255, 28, 39, 81),
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.share),
-                  onPressed: () {},
-                ),
+                    )),
+                Text("Course Details",
+                    style: GoogleFonts.spaceMono(
+                        fontSize: 30, fontWeight: FontWeight.bold)),
                 IconButton(
                   icon: Icon(Icons.more_vert),
                   onPressed: () {},
