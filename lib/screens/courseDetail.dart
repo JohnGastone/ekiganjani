222f30a3925ae9f33179da04509f819769dc9975
JohnGastone <johngastone11@gmail.com>
2024-06-06 23:22:39 +0300

    +++

--- lib/screens/courseDetail.dart
+++ lib/screens/courseDetail.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,32 +16,52 @@ class _CoursedetailsState extends State<Coursedetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                CircleAvatar(
-                    backgroundColor: const Color.fromARGB(60, 214, 209, 209),
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: Color.fromARGB(255, 28, 39, 81),
-                    )),
-                Text("Course Details",
-                    style: GoogleFonts.spaceMono(
-                        fontSize: 30, fontWeight: FontWeight.bold)),
-                IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: () {},
-                ),
-              ],
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  CircleAvatar(
+                      backgroundColor: const Color.fromARGB(60, 214, 209, 209),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Color.fromARGB(255, 28, 39, 81),
+                      )),
+                  Text("Course Details",
+                      style: GoogleFonts.spaceMono(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 28, 39, 81),
+                      )),
+                  CircleAvatar(
+                      backgroundColor: const Color.fromARGB(60, 214, 209, 209),
+                      child: Icon(
+                        CupertinoIcons.heart,
+                        color: Color.fromARGB(255, 28, 39, 81),
+                      )),
+                ],
+              ),
             ),
-          )
-        ],
-      ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 150,
+              width: 300,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      image: AssetImage("./images/image.png"),
+                      fit: BoxFit.cover)),
+            ),
+            Text(
+              "UI/UX Design Course",
+              style: GoogleFonts.spaceMono(fontSize: 25),
+            )
+          ]),
     );
   }
 }
