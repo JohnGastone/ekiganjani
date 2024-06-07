405dd11d024e8e1ed79d74f808d48fd0aef689a1
JohnGastone <johngastone11@gmail.com>
2024-06-07 23:51:49 +0300

    +++

--- lib/screens/landingScreen.dart
+++ lib/screens/landingScreen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:ekiganjani/screens/courseDetail.dart';
+import 'package:ekiganjani/screens/ourCoursesScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -108,7 +108,7 @@ class _LandingscreenState extends State<Landingscreen> {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => Coursedetails()));
+                                    builder: (context) => OurCoursesScreen()));
                           },
                           child: Text(
                             "Get Started",
--- lib/screens/ourCoursesScreen.dart
+++ lib/screens/ourCoursesScreen.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OurCoursesScreen extends StatefulWidget {
   const OurCoursesScreen({super.key});
@@ -14,9 +16,30 @@ class _OurCoursesScreenState extends State<OurCoursesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
-        title: Row(
-          children: [],
+        title: Padding(
+          padding: const EdgeInsets.only(top: 15, right: 15, left: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white24,
+                child: Icon(CupertinoIcons.person),
+              ),
+              Text(
+                "Our Courses",
+                style: GoogleFonts.spaceMono(
+                    fontSize: 19,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.white24,
+                child: Icon(Icons.search),
+              ),
+            ],
+          ),
         ),
       ),
       body: SingleChildScrollView(
