3b3f0b9af3a493f3426b2a6dcaf5f8e0839a3e8f
JohnGastone <johngastone11@gmail.com>
2024-06-08 01:00:00 +0300

    +++

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,6 +14,11 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [Icon(CupertinoIcons.person)],
+        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
