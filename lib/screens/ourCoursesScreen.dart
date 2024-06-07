22288d5c6c56862a64e532fde4b5ea277a9b300d
JohnGastone <johngastone11@gmail.com>
2024-06-07 23:43:50 +0300

    +++

---
+++ lib/screens/ourCoursesScreen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class OurCoursesScreen extends StatefulWidget {
+  const OurCoursesScreen({super.key});
+
+  @override
+  State<OurCoursesScreen> createState() => _OurCoursesScreenState();
+}
+
+class _OurCoursesScreenState extends State<OurCoursesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
