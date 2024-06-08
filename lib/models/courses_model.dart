da6b31fdbe1187e8f5dd76d2bb63be3c10523782
JohnGastone <johngastone11@gmail.com>
2024-06-08 11:05:47 +0300

    +++

---
+++ lib/models/courses_model.dart
@@ -0,0 +1,9 @@
+class CoursesModel {
+  String? courseName;
+  String? courseInstructor;
+  String? courseVideos;
+  String? progress;
+
+  CoursesModel(
+      this.courseName, this.courseInstructor, this.courseVideos, this.progress);
+}
