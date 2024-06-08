c28055e54a1e89a4306c211fbc32177b84f01aa4
JohnGastone <johngastone11@gmail.com>
2024-06-08 09:28:15 +0300

    +++

---
+++ lib/models/chats_model.dart
@@ -0,0 +1,9 @@
+class ChatsModel {
+  String? name;
+  String? chatPlaceholder;
+  String? chatTime;
+  int? unreadCount;
+
+  ChatsModel(
+      {this.name, this.chatPlaceholder, this.chatTime, this.unreadCount});
+}
