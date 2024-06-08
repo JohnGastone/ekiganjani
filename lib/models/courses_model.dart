59dd2c7c22041035e7e1b554ed67a04b9075f505
JohnGastone <johngastone11@gmail.com>
2024-06-08 11:15:19 +0300

    +++

--- lib/models/courses_model.dart
+++ lib/models/courses_model.dart
@@ -9,5 +9,32 @@ class CoursesModel {
 }
 
 class CourseList {
-  static List<CoursesModel> getAllCourses = [];
+  static List<CoursesModel> getAllCourses = [
+    CoursesModel(
+      "UI/UX Design",
+      "Omary Mkuse",
+      "11/15 Videos",
+      "75%",
+    ),
+    CoursesModel(
+      "Digital Marketing",
+      "Juma Kilumbi",
+      "12/15 Videos",
+      "80%",
+    ),
+    CoursesModel(
+      "Video Editing",
+      "Omary Mkuse",
+      "20/20 Videos",
+      "100%",
+    ),
+    CoursesModel(
+      "App Development",
+      "John Lugendo",
+      "10/30 Videos",
+      "33%",
+    ),
+  ];
+
+  static List<CoursesModel> displayCourses = List.from(getAllCourses);
 }
