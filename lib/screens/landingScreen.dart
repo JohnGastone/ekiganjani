8a2c1b55f24566c6f3a3e770d8243d384dc9f011
JohnGastone <johngastone11@gmail.com>
2024-06-08 11:48:39 +0300

    +++

--- lib/screens/landingScreen.dart
+++ lib/screens/landingScreen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:ekiganjani/screens/courseDetail.dart';
-import 'package:ekiganjani/screens/ourCoursesScreen.dart';
+import 'package:ekiganjani/screens/homeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -109,7 +108,7 @@ class _LandingscreenState extends State<Landingscreen> {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => Coursedetails()));
+                                    builder: (context) => MyHomePage()));
                           },
                           child: Text(
                             "Get Started",
