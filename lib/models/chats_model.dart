cf43e7eff29d4f27de4dee7c86c6ee2c09edf625
JohnGastone <johngastone11@gmail.com>
2024-06-08 09:35:20 +0300

    +++

--- lib/models/chats_model.dart
+++ lib/models/chats_model.dart
@@ -4,6 +4,17 @@ class ChatsModel {
   String? chatTime;
   int? unreadCount;
 
-  ChatsModel(
-      {this.name, this.chatPlaceholder, this.chatTime, this.unreadCount});
+  ChatsModel(this.name, this.chatPlaceholder, this.chatTime, this.unreadCount);
+}
+
+class ChatList {
+  static List<ChatsModel> getChats = [
+    ChatsModel("Kathleen Lugendo", "I just enrolled...", "10:00 PM", 2),
+    ChatsModel("Kathleen Lugendo", "I just enrolled...", "10:00 PM", 2),
+    ChatsModel("Kathleen Lugendo", "I just enrolled...", "10:00 PM", 2),
+    ChatsModel("Kathleen Lugendo", "I just enrolled...", "10:00 PM", 2),
+    ChatsModel("Kathleen Lugendo", "I just enrolled...", "10:00 PM", 2),
+  ];
+
+  static List<ChatsModel> displayChats = List.from(ChatList.getChats);
 }
