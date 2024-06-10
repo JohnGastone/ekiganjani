4a2464aadbba7105875f8ebd72a3d29acb8387ec
JohnGastone <johngastone11@gmail.com>
2024-06-10 11:56:06 +0300

    +++

--- lib/screens/courseDetail.dart
+++ lib/screens/courseDetail.dart
@@ -281,64 +281,7 @@ class _CoursedetailsState extends State<Coursedetails> {
                                 ],
                               ),
                             )),
-                      )
-                          // child: Card(
-                          //     color: Colors.white24,
-                          //     elevation: 5,
-                          //     shape: RoundedRectangleBorder(
-                          //       borderRadius: BorderRadius.circular(15),
-                          //     ),
-                          //     child: Padding(
-                          //       padding: EdgeInsets.all(15),
-                          //       child: Row(
-                          //         children: [
-                          //           Container(
-                          //             height: 80,
-                          //             width: 80,
-                          //             decoration: BoxDecoration(
-                          //                 borderRadius: BorderRadius.circular(10),
-                          //                 image: DecorationImage(
-                          //                     image: AssetImage(
-                          //                         "./images/image.png"),
-                          //                     fit: BoxFit.cover)),
-                          //           ),
-                          //           SizedBox(
-                          //             width: 10,
-                          //           ),
-                          //           Column(
-                          //             crossAxisAlignment:
-                          //                 CrossAxisAlignment.start,
-                          //             children: [
-                          //               Text(
-                          //                 getModules[index].moduleName!,
-                          //                 style: GoogleFonts.spaceMono(
-                          //                     fontSize: 16,
-                          //                     color:
-                          //                         Color.fromARGB(255, 18, 58, 93),
-                          //                     fontWeight: FontWeight.bold),
-                          //               ),
-                          //               SizedBox(
-                          //                 height: 15,
-                          //               ),
-                          //               Text(
-                          //                 getModules[index].moduleDuration!,
-                          //                 style: GoogleFonts.spaceMono(
-                          //                     fontSize: 16,
-                          //                     color: Colors.grey,
-                          //                     fontWeight: FontWeight.w400),
-                          //               )
-                          //             ],
-                          //           ),
-                          //           Spacer(),
-                          //           Image.asset(
-                          //             ("./images/progress.png"),
-                          //             height: 50,
-                          //             width: 50,
-                          //           )
-                          //         ],
-                          //       ),
-                          //     )),
-                          )),
+                      ))),
             ),
             SizedBox(
               height: 50,
